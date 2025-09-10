e73c7ea1ab3e53c76939d9dc22b203a9993ebdb7
Shorifur Rahman <shorifurrahman566@gmail.com>
2025-09-10 08:29:07 +0300

    New UPdate

--- server/routes/social/index.js
+++ server/routes/social/index.js
@@ -1,14 +1,26 @@
-// In server/routes/social/index.js
+// server/routes/social/index.js
 const express = require('express');
 const router = express.Router();
-const followRoutes = require('./follow');
-const postRoutes = require('./posts'); // If you have this
-const storyRoutes = require('./stories'); // If you have this
 
-// Use follow routes
-router.use('/follow', followRoutes);
-router.use('/posts', postRoutes);
-router.use('/stories', storyRoutes);
+// Import all social sub-routes
+const postsRouter = require('./posts');
+const reactionsRouter = require('./reactions');
+const commentsRouter = require('./comments');
+const sharesRouter = require('./shares');
+const storiesRouter = require('./stories');
+const followRouter = require('./follow');
+
+// Use all social sub-routes
+router.use('/posts', postsRouter);
+router.use('/reactions', reactionsRouter);
+router.use('/comments', commentsRouter);
+router.use('/shares', sharesRouter);
+router.use('/stories', storiesRouter);
+router.use('/follow', followRouter);
+
+// Social root endpoint
+router.get('/', (req, res) => {
+  res.json({ message: 'Social API endpoints' });
+});
 
 module.exports = router;
-
